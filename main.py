d61471ef0bc154bdcb78f7bc5e69105ca612c028
chandana-17c <chandana172006@gmail.com>
2026-07-28 01:11:09 +0530

    Fix CORS for Render frontend

--- main.py
+++ main.py
@@ -35,17 +35,19 @@ app = FastAPI(
     version="1.0"
 )
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "https://echosocial-ai-frontend.onrender.com",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # ==========================================
 # LOGIN
 # ==========================================
